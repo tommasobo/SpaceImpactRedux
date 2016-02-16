3c1d5177d054c428d741d333c42427c5417d097c
Nicola Cielo Lugaresi Secci <nicola.lugaresisecci@studio.unibo.it>
2016-02-16 12:17:23 +0100

    GameLoop created

--- src/spaceimpact/controller/Controller.java
+++ src/spaceimpact/controller/Controller.java
@@ -1,39 +1,50 @@
 package spaceimpact.controller;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 import spaceimpact.utilities.Pair;
 
 public class Controller implements ControllerInterface {
 	private static final String HS_FILENAME = "hiscores";
 	private static final int HS_NSCORES = 10;
-	
+	private static final int FPS = 30;
+	private static Controller c;
 	
 	private final HighScoresManager hsManager;
-	
+	private Optional<GameLoop> gl;
 	
 	private Controller() {
 		this.hsManager = new HighScoresManager(Controller.HS_FILENAME, Controller.HS_NSCORES);
+		this.gl = Optional.empty();
 	}
 	
 	
 	@Override
 	public void startGameLoop() throws IllegalStateException {
-		// TODO Auto-generated method stub
+		this.gl.ifPresent(g -> {
+			throw new IllegalStateException();
+		});
+		GameLoop game = new GameLoop(Controller.FPS);
+		this.gl = Optional.of(game);
+		game.start();
 		
 	}
 
 	@Override
 	public void abortGameLoop() throws IllegalStateException {
-		// TODO Auto-generated method stub
-		
+		this.gl.orElseThrow( () -> new IllegalStateException());
+		this.gl.get().abort();
 	}
 
 	@Override
 	public List<Pair<String, Integer>> getCurrentHighScores() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.hsManager.getScores();
 	}
 
+	public static void main(String args[]) {
+		Controller.c = new Controller();
+	}
 	
 }
