77da7052fc887f281e9206e71e4629a5cb718bb8
Davide Giacomini <davide.giacomini2@studio.unibo.it>
2016-02-19 14:53:41 +0100

    Added Direction implementation and modified all related classes. Refactored weapon interface. Updated Test JUnit class.

--- src/spaceimpact/model/Test.java
+++ src/spaceimpact/model/Test.java
@@ -4,21 +4,28 @@ import static org.junit.Assert.*;
 import spaceimpact.model.entities.*;
 import java.awt.Rectangle;
 
+/**
+ * JUnit test class for Model classes
+ * @author Davide
+ */
 public class Test {
 
+	Spaceship ship = new Spaceship(100, 0.1, new Location(10, 10, new Rectangle(2, 2)), Direction.N);
+	Entity enemy = new Enemy(100, 0.1, new Location(8, 8, new Rectangle(4, 2)), Direction.N);
+	
 	@org.junit.Test
-	public void testCollision() {
-		
-		Entity ship = new Spaceship(100, 0.1, 100);	
-		ship.setLocation(new Location(10, 10, new Rectangle(2, 2)));
-		
-		Entity enemy = new Enemy(100, 0.1, 100);
-		enemy.setLocation(new Location(8, 8, new Rectangle(4, 2)));
-			
+	public void testCollision() {		
 		assertFalse(ship.collideWith(enemy));
 		enemy.update();	
 		assertTrue(enemy.collideWith(ship));
 		ship.update();
 		assertFalse(ship.collideWith(enemy));
+	}
+	
+	@org.junit.Test
+	public void testGeneral() {
+		assertFalse(ship.getID().equals(enemy.getID()));
+		assertTrue(ship.getID().equals(EntityType.Spaceship));
+		ship.setWeapon(new Weapon(ship.getID(), ship.getLocation(), 5));
 	}
 }
---
+++ src/spaceimpact/model/entities/WeaponInterface.java
@@ -0,0 +1,25 @@
+package spaceimpact.model.entities;
+
+/** 
+ * Weapon Interface
+ * <br>
+ * Basically a projectiles factory
+ * @author Davide
+ */
+public interface WeaponInterface {
+
+	/** Shoot a Projectile
+	 * <br>
+	 * Create and shoot a new Projectile
+	 * @param direction of the projectile
+	 * @return A new projective object
+	 */
+	Projectile shoot(Direction direction);
+	
+	/** Enhance weapon capabilities
+	 * <br>
+	 * @param damageincrement Projectiles damage
+	 * @param projectilesvelocity Velocity of shooted projectiles
+	 */
+	void enhance(int damageincrement, double projectilesvelocity);	
+}
