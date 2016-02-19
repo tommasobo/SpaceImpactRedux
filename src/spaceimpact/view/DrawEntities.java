50a22e7774f009218a543181607cbc4734e0bbd4
Nicola Cielo Lugaresi Secci <nicola.lugaresisecci@studio.unibo.it>
2016-02-19 01:12:11 +0100

    Edited GameLoop, ImageLoader and DrawEntities

--- src/spaceimpact/view/DrawEntities.java
+++ src/spaceimpact/view/DrawEntities.java
@@ -2,9 +2,8 @@ package spaceimpact.view;
 
 import java.util.List;
 
-import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import spaceimpact.model.Location;
 import spaceimpact.utilities.ImageLoader;
 import spaceimpact.utilities.Pair;
@@ -16,14 +15,14 @@ public class DrawEntities {
 		this.imgl = ImageLoader.getLoader();
 	}
 
-	public void draw(final Scene scene, final List<Pair<String, Location>> listEntities) {
-		scene.setOnKeyPressed(InputHandler.getInputHandler());
-		final HBox layout = new HBox();
+	public void draw(final Pane layer, final List<Pair<String, Location>> listEntities) {
 		listEntities.forEach(p -> {
 			final ImageView iv = new ImageView(this.imgl.getImageFromPath(p.getFirst()));
 			iv.setFitHeight(50);
 			iv.setFitWidth(60);
-			layout.getChildren().add(iv);
+			layer.getChildren().add(iv);
+			iv.setX(p.getSecond().getX());
+			iv.setY(p.getSecond().getY());
 		});
 
 	}
