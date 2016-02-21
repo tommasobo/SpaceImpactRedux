d5e8d7e059b7b992e9e2baf2349a372af1fcab9c
Tommaso Bonato <tommaso.bonato@studio.unibo.it>
2016-02-21 18:03:53 +0100

    Added a method to update player's info

---
+++ src/spaceimpact/view/PlayerInfo.java
@@ -0,0 +1,12 @@
+package spaceimpact.view;
+
+import javafx.scene.control.Label;
+
+public class PlayerInfo {
+
+    public void update(Label hp, Label shields, int hp2, int shields2, int score) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
