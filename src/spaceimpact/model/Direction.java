842eff368e9a5f331a74846ceb0d595b94a2ed51
Davide Giacomini <davide.giacomini2@studio.unibo.it>
2016-02-22 12:05:38 +0100

    Updated Direction enum with other direction. Still need to modify move method inside spaceship.

--- src/spaceimpact/model/Direction.java
+++ src/spaceimpact/model/Direction.java
@@ -14,19 +14,37 @@ package spaceimpact.model;
 public enum Direction {
  
 	N, 
-	S,
+	NE,
 	E,
-	W;
+	SE,
+	S,
+	SW,
+	W,
+	NW;
+	
+	private final double diagonalvel = 0.7071; //diagonal velocity
 	
 	public void moveLocation(Location loc, double v) {
 		if (this.equals(Direction.N)) {
 			loc.setY(loc.getY() - v); 
+		} else if (this.equals(Direction.NE)) {
+			Direction.N.moveLocation(loc, v * diagonalvel);
+			Direction.E.moveLocation(loc, v * diagonalvel);  
 		} else if (this.equals(Direction.S)) {
 			loc.setY(loc.getY() + v); 
+		} else if (this.equals(Direction.SE)) {
+			Direction.S.moveLocation(loc, v * diagonalvel); 
+			Direction.E.moveLocation(loc, v * diagonalvel);
 		} else if (this.equals(Direction.E)) {
 			loc.setX(loc.getX() + v); 
+		} else if (this.equals(Direction.SW)) {
+			Direction.S.moveLocation(loc, v * diagonalvel);
+			Direction.W.moveLocation(loc, v * diagonalvel);  
 		} else if (this.equals(Direction.W)) {
 			loc.setX(loc.getX() - v); 
+		} else if (this.equals(Direction.NW)) {
+			Direction.N.moveLocation(loc, v * diagonalvel);
+			Direction.W.moveLocation(loc, v * diagonalvel);  
 		}
 	};
 }
