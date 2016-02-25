5021892b3bd8b1b80f5d082884294ca2564e29a2
Nicola Cielo Lugaresi Secci <nicola.lugaresisecci@studio.unibo.it>
2016-02-25 13:06:13 +0100

    Changes to EntityType: cleaner code (requires changes to Debris, Direction and PowerUp)

--- src/spaceimpact/model/entities/EntityType.java
+++ src/spaceimpact/model/entities/EntityType.java
@@ -1,8 +1,8 @@
 package spaceimpact.model.entities;
 
-import spaceimpact.model.Direction;
-import spaceimpact.model.entities.Debris.DebrisType;
-import spaceimpact.model.entities.PowerUp.Enhancement;
+import java.util.Arrays;
+import java.util.List;
+
 import spaceimpact.utilities.Pair;
 
 /**
@@ -10,14 +10,25 @@ import spaceimpact.utilities.Pair;
  * List of possibile types:<br>
  * <b>Spaceship</b> As the player Entity<br>
  * <b>Enemy</b> The entities which the player has to destroy<br>
- * <b>Debris</b> The entities spawned in a collision or randomly as asteroids<br>
+ * <b>Debris</b> The entities spawned in a collision or randomly as asteroids
+ * <br>
  * <b>PowerUp</b> The entities with which the player can obtain ehnacements<br>
- * <b>Projectile</b> The entities spawned by a weapon. Can damage other living entities<br>
+ * <b>Projectile</b> The entities spawned by a weapon. Can damage other living
+ * entities<br>
  *
  * @author Davide
  */
 public enum EntityType {
 	Spaceship, Enemy, Debris, PowerUp, Projectile;
+
+	private static List<Pair<String, Integer>> projectilesRange = Arrays.asList(new Pair<>("diagonal-green.png", 15),
+			new Pair<>("beam_blue.png", 25), new Pair<>("beam_red.png", 35), new Pair<>("fireball.png", 50));
+
+	private static List<Pair<String, Integer>> enemyDmgRange = Arrays.asList(new Pair<>("A", 15), new Pair<>("B", 30),
+			new Pair<>("C", 45));
+
+	private static List<Pair<String, Integer>> enemyLifeRange = Arrays.asList(new Pair<>("1.png", 12),
+			new Pair<>("2.png", 24), new Pair<>("3.png", 36), new Pair<>("4.png", 48), new Pair<>("5.png", 60));
 
 	/**
 	 * Returns data to print the image of the entity. <br>
@@ -32,86 +43,29 @@ public enum EntityType {
 		double rotation = 0;
 		if (e instanceof Projectile) {
 			s.append("Projectiles/");
-			final Projectile p = (Projectile) e;
-			boolean ignoreOrizRot = true;
-			if (p.getDamage() <= 5) {
-				s.append("diagonal-green.png");
-				ignoreOrizRot = false;
-			} else if (p.getDamage() <= 10) {
-				s.append("beam_blue.png");
-			} else if (p.getDamage() < 20) {
-				s.append("beam_red.png");
-			} else {
-				s.append("fireball.png");
-				ignoreOrizRot = false;
-			}
-			if (p.getDirection() == Direction.N) {
-				rotation = 270;
-			} else if (p.getDirection() == Direction.S) {
-				rotation = 90;
-			} else if (!ignoreOrizRot && (p.getDirection() == Direction.W)) {
-				rotation = 180;
-			} else if (p.getDirection() == Direction.SE) {
-				rotation = 45;
-			} else if (p.getDirection() == Direction.SW) {
-				rotation = 135;
-			} else if (p.getDirection() == Direction.NW) {
-				rotation = 225;
-			} else if (p.getDirection() == Direction.NE) {
-				rotation = 315;
-			}
+			s.append(EntityType.textChooser(((Projectile) e).getDamage(), EntityType.projectilesRange));
+			rotation = ((Projectile) e).getDirection().getRotation();
 		} else if (e instanceof Enemy) {
 			s.append("Enemies/");
-			final Enemy en = (Enemy) e;
-			final int maxlife = en.getMaximumLife();
-			final int dmg = en.getWeapon().getDamage();
-			if (dmg <= 10) {
-				s.append('A');
-			} else if (dmg < 20) {
-				s.append('B');
-			} else {
-				s.append('C');
-			}
-			if (maxlife <= 10) {
-				s.append("1.png");
-			} else if (maxlife <= 20) {
-				s.append("2.png");
-			} else if (maxlife <= 30) {
-				s.append("3.png");
-			} else if (maxlife <= 40) {
-				s.append("4.png");
-			} else {
-				s.append("5.png");
-			}
+			s.append(EntityType.textChooser(((Enemy) e).getWeapon().getDamage(), EntityType.enemyDmgRange));
+			s.append(EntityType.textChooser(((Enemy) e).getMaximumLife(), EntityType.enemyLifeRange));
 		} else if (e instanceof Debris) {
-			final Debris d = (Debris) e;
 			s.append("Debris/");
-			if (d.getType() == DebrisType.Explosion) {
-				s.append("explosion.gif");
-			} else if (d.getType() == DebrisType.Hit) {
-				s.append("hit.gif");
-			} else if (d.getType() == DebrisType.Sparkle) {
-				s.append("sparkle.gif");
-			} else {
-				s.append("asteroid.gif");
-			}
+			s.append(((Debris) e).getType().getImageName());
 		} else if (e instanceof PowerUp) {
-			final PowerUp p = (PowerUp) e;
 			s.append("Powerups/");
-			if (p.getEnhancement() == Enhancement.AddProjectile) {
-				s.append("addprojectile.gif");
-			} else if (p.getEnhancement() == Enhancement.CoolDownDecreased) {
-				s.append("cooldown.gif");
-			} else if (p.getEnhancement() == Enhancement.IncrementDamage) {
-				s.append("damage.gif");
-			} else if (p.getEnhancement() == Enhancement.RestoreShield) {
-				s.append("shield.gif");
-			} else if (p.getEnhancement() == Enhancement.IncrementSpeed) {
-				s.append("speed.gif");
-			} else {
-				s.append("heal.gif");
-			}
+			s.append(((PowerUp) e).getEnhancement().getImageName());
 		}
 		return new Pair<String, Double>(s.toString(), Double.valueOf(rotation));
+	}
+
+	private static String textChooser(final int param, final List<Pair<String, Integer>> ranges) {
+		for (final Pair<String, Integer> p : ranges) {
+			if (param < p.getSecond()) {
+				return p.getFirst();
+			}
+		}
+		return ranges.get(ranges.size() - 1).getFirst();
+
 	}
 }
